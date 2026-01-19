bddf87f6e7eb2bec9b7b7b3fa9974d8228033f12
mohammed6666dz-create <mohammed6666dz@gmail.com>
2026-01-19 16:10:30 +0100

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -678,7 +678,7 @@ let myPoints = 1; // القيمة الافتراضية لأول مرة
 let myLevel = 1;
 
 // صوت الطاق (عصفور)
-const mentionSound = new Audio('https://assets.codepen.io/605876/bird-chirp-short.mp3');
+const mentionSound = new Audio('./bird-chirp-short.mp3');
 mentionSound.volume = 0.7;
 
 socket.emit('join', room, token);
